5257cb898a8a07331ea836a37a49a05958c34f42
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-29 00:31:04 +0600

    Data Conversion in JavaScript

---
+++ app.js
@@ -0,0 +1,20 @@
+let example;
+
+//number to string
+
+example = 4545;
+
+//way to string
+
+example = String(4545);
+
+example = String(5 + 5);
+example = String(55555);
+
+example = new Date();
+example = String(new Date());
+
+//output
+console.log(example);
+console.log(typeof example);
+console.log(example.length);
