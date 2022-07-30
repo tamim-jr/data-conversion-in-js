eea4553eba27840f04097badb68d206b37d06943
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-31 00:08:53 +0600

    type conversation advance

--- app.js
+++ app.js
@@ -18,3 +18,30 @@ example = String(new Date());
 console.log(example);
 console.log(typeof example);
 console.log(example.length);
+
+//number to string
+myAge = 29;
+myAge = (29).toString();
+console.log(typeof myAge);
+console.log(myAge);
+
+//number to string
+myPhone = "555";
+myName = Number("555");
+console.log(myPhone);
+console.log(typeof myPhone);
+
+//boolean to string
+result = true;
+result = Number(true);
+console.log(result);
+
+result = false;
+result = Number(false);
+console.log(result);
+
+//PartsInt
+
+allMoney = 451842.1542845;
+allMoney = parseInt(451842.1542845);
+console.log(allMoney);
